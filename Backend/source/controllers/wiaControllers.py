01e00f3f586ba60f161f3b1f334af91d9bd2ba1d
dev-gaurav <gaurav.singh@flairminds.com>
2024-12-12 14:44:57 +0530

    service response added

--- Backend/source/controllers/wiaControllers.py
+++ Backend/source/controllers/wiaControllers.py
@@ -190,6 +190,11 @@ def update_values_in_sheet():
         updated_df, initial_df = wiaService.update_add_df(data)
 
         response_data = wiaService.save_updated_df(data, updated_df, initial_df)
-        return HTTPResponse.success(result=response_data)
+        if not response_data["success"]:
+            return HTTPResponse.error(message="Internal Server Error")
+        
+        return HTTPResponse.success(result={
+            "modified_base_data_file_id": response_data["data"]["modified_base_data_file_id"]
+        })
     except Exception as e:
         return HTTPResponse.error(message="Internal Server Error")
--- Backend/source/services/commons/commonServices.py
+++ Backend/source/services/commons/commonServices.py
@@ -1,5 +1,7 @@
 from models import BaseDataFile
-
+import numpy as np
+import pandas as pd
+from source.utility.ServiceResponse import ServiceResponse
 
 def get_base_data_file(**kwargs):
     if "base_data_file_id" in kwargs.keys():
@@ -19,3 +21,74 @@ def get_base_data_file(**kwargs):
                 closing_date=closing_date, user_id=user_id
             ).first()
     return base_data_file
+
+
+def get_raw_value(updated_value, col_type):
+    try:
+        if col_type == np.float64:
+            if updated_value != "":
+                if type(updated_value) == str:
+                    updated_value = updated_value.replace(",", "")
+                    updated_value = float(updated_value)
+            else:
+                updated_value = None
+        if col_type == np.int64:
+            if updated_value != "":
+                if type(updated_value) == str:
+                    updated_value = updated_value.replace(",", "")
+                    updated_value = int(updated_value)
+            else:
+                updated_value = None
+        elif col_type == "<M8[ns]":  # Handling datetime
+            if updated_value != "":
+                if type(updated_value) != str:
+                    if find_is_NaT(updated_value):
+                        updated_value = ""
+                    else:
+                        updated_value = pd.to_datetime(
+                            updated_value, errors="coerce"
+                        ).strftime("%Y-%m-%d")
+            else:
+                updated_value = None
+        if col_type == object:
+            try:
+                if not pd.isna(updated_value):
+                    if updated_value != "":
+                        updated_value = float(updated_value.replace(",", ""))
+                    else:
+                        updated_value = None
+                else:
+                    updated_value = ""
+            except ValueError:
+                updated_value = updated_value
+        return updated_value
+    
+    except Exception as e:
+        return ServiceResponse.error(message=f"Error in get_raw_value: {e}")
+
+
+def get_row_index(sheet_df, row_name):
+    try:
+        row_index = sheet_df[sheet_df[sheet_df.columns[0]] == row_name].index[0]
+        return row_index
+    except IndexError as ie:
+        return -1
+    
+def find_is_NaT(previous_value):
+    try:
+        if pd.isna(previous_value):
+            return True
+    except Exception as e:
+        return False
+
+
+def get_updated_value(updated_value):
+    if isinstance(updated_value, str):
+        try:
+            if updated_value.endswith("%"):
+                updated_value = updated_value[:-1]
+                updated_value = float(updated_value) / 100
+        except ValueError:
+            pass
+    return updated_value
+
