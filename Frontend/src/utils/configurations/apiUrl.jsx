d63c61a5400aa7f47d8ee26ddc7570feb06825a7
varadsc <varadchitnis01@gmail.com>
2025-01-28 17:22:24 +0530

    Archive feature added

--- Frontend/src/utils/configurations/apiUrl.jsx
+++ Frontend/src/utils/configurations/apiUrl.jsx
@@ -12,7 +12,7 @@ const apiURL = 'https://4w5k1fp7-5000.inc1.devtunnels.ms/api';
 //const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 
 // const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
-// const apiURL = 'http://127.0.0.1:5000/api';
+const apiURL = 'http://127.0.0.1:5000/api';
 
 // const apiURL = 'http://localhost:5000/api';
 
