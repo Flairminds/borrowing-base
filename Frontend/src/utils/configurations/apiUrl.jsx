e51664a9719ec29789d3c38ef970504443ff5abd
Mukesh <mukeshbhairewar96@gmail.com>
2025-01-30 15:09:26 +0530

    Apply Button Disabled.

--- Frontend/src/utils/configurations/apiUrl.jsx
+++ Frontend/src/utils/configurations/apiUrl.jsx
@@ -1,6 +1,6 @@
 
 // Varad
-const apiURL = 'https://4w5k1fp7-5000.inc1.devtunnels.ms/api';
+//const apiURL = 'https://4w5k1fp7-5000.inc1.devtunnels.ms/api';
 
 // // Gaurav
 // const apiURL = 'https://qgt25b0c-5000.inc1.devtunnels.ms/api';
@@ -9,7 +9,7 @@ const apiURL = 'https://4w5k1fp7-5000.inc1.devtunnels.ms/api';
 //const apiURL = 'https://ts3pl306-5000.inc1.devtunnels.ms/api';
 
 // Deployed link
-//const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
+const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 
 //const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 // const apiURL = 'http://127.0.0.1:5000/api';
