aacbbf04aaa0dcb9f2349f904f94bd7d22985dc9
varadsc <varadchitnis01@gmail.com>
2025-02-03 12:36:31 +0530

    Input component added

--- Frontend/src/utils/configurations/apiUrl.jsx
+++ Frontend/src/utils/configurations/apiUrl.jsx
@@ -9,14 +9,14 @@
 //const apiURL = 'https://ts3pl306-5000.inc1.devtunnels.ms/api';
 
 // Deployed link
-const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
+// const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 
 //const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 
 // const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 // const apiURL = 'http://127.0.0.1:5000/api';
 
-// const apiURL = 'http://localhost:5000/api';
+const apiURL = 'http://localhost:5000/api';
 
 // local backend link
 // const apiURL = `${window.location}/api`;
