b4877c519c3ce51f27cd75c1ad677034abc6c92f
punit-fm <punit.suman@flairminds.com>
2025-02-25 22:08:46 +0530

    base data ui fixes

--- Frontend/src/components/loader/loader.jsx
+++ Frontend/src/components/loader/loader.jsx
@@ -11,6 +11,17 @@ export const Loader = () => (
 	</div>
 );
 
+export const LoaderFullPage = () => (
+	<div style={{position: 'absolute', top: '0', left: '0', backgroundColor: 'white', opacity: '0.4', width: '100vw', height: '100vh', zIndex: '5000'}}>
+		{/* <Spin indicator={<LoadingOutlined spin />} size="small" /> */}
+		{/* <Spin indicator={<LoadingOutlined spin />} /> */}
+		{/* <Spin indicator={<LoadingOutlined spin />} size="large" /> */}
+		<div style={{marginTop: '15%', opacity: '1'}}>
+			<Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+		</div>
+	</div>
+);
+
 export const LoaderSmall = () => (
 	<div>
 		{/* <Spin indicator={<LoadingOutlined spin />} size="small" /> */}
