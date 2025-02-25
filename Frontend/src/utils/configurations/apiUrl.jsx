c9e469fa8fca766031ce48ac97cba00c5a05a834
abhinavFM <abhinav.vaidya@flairminds.com>
2025-02-25 15:18:05 +0530

    conflict resolved

--- Frontend/src/utils/configurations/apiUrl.jsx
+++ Frontend/src/utils/configurations/apiUrl.jsx
@@ -16,7 +16,7 @@ const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 // const apiURL = 'https://borrowing-base-uat.azurewebsites.net/api';
 // const apiURL = 'http://127.0.0.1:5000/api';
 
-const apiURL = 'http://localhost:5000/api';
+// const apiURL = 'http://localhost:5000/api';
 
 // local backend link
 // const apiURL = `${window.location}/api`;
