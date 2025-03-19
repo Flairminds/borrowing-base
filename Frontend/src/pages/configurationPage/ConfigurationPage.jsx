cdc5dfa471a4b678c1b7fcc7541900c432e08928
varadsc <varadchitnis01@gmail.com>
2025-03-19 18:09:16 +0530

    generic loan and lien type from frontend

--- Frontend/src/pages/configurationPage/ConfigurationPage.jsx
+++ Frontend/src/pages/configurationPage/ConfigurationPage.jsx
@@ -1,12 +1,16 @@
 import { Tabs } from 'antd';
-import React from 'react';
-import { configurationTabs } from '../../utils/constants/configurationConstants';
+import React, { useState } from 'react';
+import { uniqueMappingValues } from '../../utils/constants/configurationConstants';
+import { GenericDragnDropMapping } from '../configurationPageTabs/genericDragnDropmapping/GenericDragnDropMapping';
 import styles from './ConfigurationPage.module.css';
 
 export const ConfigurationPage = () => {
+	const [activeMappingType, setActiveMappingType] = useState(uniqueMappingValues["2"]);
 
 	const onChange = (key) => {
-		console.info(key);
+		if (uniqueMappingValues[key]) {
+			setActiveMappingType(uniqueMappingValues[key]);
+		}
 	};
 
 	return (
@@ -14,7 +18,23 @@ export const ConfigurationPage = () => {
 			<Tabs
 				type="card"
 				defaultActiveKey="2"
-				items={configurationTabs}
+				items={[
+					{
+						key: "1",
+						label: "Security Mapping",
+						children: "Security Mapping UI"
+					},
+					{
+						key: "2",
+						label: "Loan Type Mapping",
+						children: <GenericDragnDropMapping activeMappingType={activeMappingType} />
+					},
+					{
+						key: "3",
+						label: "Lien Type Mapping",
+						children: <GenericDragnDropMapping activeMappingType={activeMappingType} />
+					}
+				]}
 				onChange={onChange}
 				className={styles.configTabs}
 			/>
