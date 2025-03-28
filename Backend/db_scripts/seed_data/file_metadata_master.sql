82e972ddfd85cb3e00a4e0d6b4556c52ba00a5f1
dev-gaurav <gaurav.singh@flairminds.com>
2025-03-28 13:56:56 +0530

    script updated for validation

--- Backend/db_scripts/seed_data/file_metadata_master.sql
+++ Backend/db_scripts/seed_data/file_metadata_master.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.file_metadata_master
 (id, company_id, created_at, created_by, modified_by, modified_at, fund_ids, "name", lookup, "type", description, is_input, is_output)
-VALUES(1, 1, '2025-03-25 14:03:16.721', 1, NULL, NULL, '{1,2}', 'MasterComp.xlsx', 'master_comp', 'master_comp', 'Master data for company', true, false);
+VALUES(1, 1, now(), 1, NULL, NULL, '{1,2}', 'MasterComp.xlsx', 'master_comp', 'master_comp', 'Master data for company', true, false);
