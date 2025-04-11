e5fec4a50a2d5b129da7af24a16862350c6f5912
dev-gaurav <gaurav.singh@flairminds.com>
2025-04-11 18:47:03 +0530

    validation for add other info

--- Backend/source/utility/HTTPResponse.py
+++ Backend/source/utility/HTTPResponse.py
@@ -1,5 +1,10 @@
 class HTTPResponse:
 
+    ERROR_CODES = {
+        "VALIDATION_FAILED": "ERR_400",
+        "SERVER_ERROR": "ERR_500",
+    }
+
     @staticmethod
     def success(result = [], message = "Successfully processed."):
         return {
@@ -9,10 +14,10 @@ class HTTPResponse:
         }, 200
 
     @staticmethod
-    def error(message = "Error encountered.", status_code = 500, error_code = 'ERR_001', result = []):
+    def error(message = "Error encountered.", status_code = 500, error_code = 'ERR_001', result = [], error='SERVER_ERROR'):
         return {
             "success": False,
-            "error_code": 'ERR_' + str(status_code),
+            "error_code": HTTPResponse.ERROR_CODES[error],
             "result": result,
             "message": message
         }, status_code
