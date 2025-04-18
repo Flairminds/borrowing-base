13f7b7a4b63d221a5aecd5280bdbf7b44246235d
punit-fm <punit.suman@flairminds.com>
2025-04-18 18:43:07 +0530

    base data mapping fixes

--- Frontend/src/pages/borrowingBasePreview/BorrowingBasePreviewPage.jsx
+++ Frontend/src/pages/borrowingBasePreview/BorrowingBasePreviewPage.jsx
@@ -257,8 +257,8 @@ export const BorrowingBasePreviewPage = ({ baseFilePreviewData, setBaseFilePrevi
 							</div>
 						</div>
 						<div>
-							<UIComponents.Button onClick={showModal} isFilled={true} text='Add Securities Data' btnDisabled={previewFundType == 'PCOF' ? false : false} title={'Add more securities data in the base data'} />
-							<UIComponents.Button onClick={() => setIsShowEmptyBaseDataModalOpen(true)} isFilled={true} text='Trigger Calculation' loading={triggerBBCalculation} loadingText={'Calculating'} btnDisabled={previewFundType == 'PCOF' ? false : false} />
+							<UIComponents.Button onClick={showModal} isFilled={true} text='Add Securities Data' btnDisabled={previewFundType == 'PSSL' ? true : false} title={previewFundType == 'PSSL' ? 'Work in progress' : 'Add more securities data in the base data'} />
+							<UIComponents.Button onClick={() => setIsShowEmptyBaseDataModalOpen(true)} isFilled={true} text='Trigger Calculation' loading={triggerBBCalculation} loadingText={'Calculating'} btnDisabled={previewFundType == 'PSSL' ? true : false} />
 						</div>
 					</div>
 					<div>
