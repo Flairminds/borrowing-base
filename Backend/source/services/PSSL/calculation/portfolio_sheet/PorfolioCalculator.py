6500b48344d1f7f3c4aed735c4a10f271dc0cacc
abhinavFM <abhinav.vaidya@flairminds.com>
2025-04-21 11:00:00 +0530

    Resolved merge conflict

--- Backend/source/services/PSSL/calculation/portfolio_sheet/PorfolioCalculator.py
+++ Backend/source/services/PSSL/calculation/portfolio_sheet/PorfolioCalculator.py
@@ -3,6 +3,8 @@ from source.services.PSSL.calculation.portfolio_sheet.EbitdaNetDebtRelevantTestP
 from source.services.PSSL.calculation.portfolio_sheet.LeverageRatiosPermittedTtmEbitda import LeverageRatiosPermittedTtmEbitda
 from source.services.PSSL.calculation.portfolio_sheet.RecurringRevenue import RecurringRevenueInterestCoverage
 from source.services.PSSL.calculation.portfolio_sheet.ValueAdjustmentEvent import ValueAdjustmentEvent
+from source.services.PSSL.calculation.portfolio_sheet.RevolverValidationCheckAndBorrower import RevolverValidationCheckAndBorrower
+from source.services.PSSL.calculation.portfolio_sheet.Others import Others
 
 class PortfolioCalculator:
     def __init__(self, calculator_info):
@@ -10,6 +12,23 @@ class PortfolioCalculator:
         exchange_rates_df = calculator_info.intermediate_calculation_dict['Exchange Rates']
         exchange_rate_map = exchange_rates_df.set_index("Currency")["Exchange Rate"].to_dict()
         self.calculator_info.intermediate_calculation_dict['exchange_rate_map'] = exchange_rate_map
+
+        obligor_tiers_df = calculator_info.intermediate_calculation_dict['Obligor Tiers']
+        obligor_tiers_map = {
+            'tier_1_applicable_value': obligor_tiers_df.query("Obligor == 'Tier 1'")['Applicable Collateral Value'][0],
+            'tier_2_applicable_value': obligor_tiers_df.query("Obligor == 'Tier 2'")['Applicable Collateral Value'][1],
+            'tier_3_applicable_value': obligor_tiers_df.query("Obligor == 'Tier 3'")['Applicable Collateral Value'][2],
+            'tier_1_1l': obligor_tiers_df.query("Obligor == 'Tier 1'")['First Lien Loans'][0],
+            'tier_1_2l': obligor_tiers_df.query("Obligor == 'Tier 1'")['FLLO/2nd Lien Loans'][0],
+            'tier_2_1l': obligor_tiers_df.query("Obligor == 'Tier 2'")['First Lien Loans'][1],
+            'tier_2_2l': obligor_tiers_df.query("Obligor == 'Tier 2'")['FLLO/2nd Lien Loans'][1],
+            'tier_3_1l': obligor_tiers_df.query("Obligor == 'Tier 3'")['First Lien Loans'][2],
+            'tier_3_2l': obligor_tiers_df.query("Obligor == 'Tier 3'")['FLLO/2nd Lien Loans'][2],
+            'tier_1_rr': obligor_tiers_df.query("Obligor == 'Tier 1'")['Recurring Revenue'][0],
+            'tier_2_rr': obligor_tiers_df.query("Obligor == 'Tier 2'")['Recurring Revenue'][1],
+            'tier_3_rr': obligor_tiers_df.query("Obligor == 'Tier 3'")['Recurring Revenue'][2],
+        }
+        self.calculator_info.intermediate_calculation_dict['obligor_tiers_map'] = obligor_tiers_map
 
     def calculate_portfolio(self):
         ebitda_net_at_date_inclussion = EbitdaNetAtDateInclussion(self.calculator_info)
@@ -22,4 +41,8 @@ class PortfolioCalculator:
         recurring_revenue.calculate_RRIC()
         value_adjustment_event = ValueAdjustmentEvent(self.calculator_info)
         value_adjustment_event.calculate_vae()
+        revolver_validation_check_borrower = RevolverValidationCheckAndBorrower(self.calculator_info)
+        revolver_validation_check_borrower.calculate_RVCBB()
+        other = Others(self.calculator_info)
+        other.calculate_others()
         print('calculation of sheet Portfolio is completed')
