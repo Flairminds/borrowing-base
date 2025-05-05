e63c29db92fdf0aa2f67cf87af12aa84660c719a
dev-gaurav <gaurav.singh@flairminds.com>
2025-05-05 12:33:06 +0530

    unammaped column

--- Backend/source/controllers/diController.py
+++ Backend/source/controllers/diController.py
@@ -399,3 +399,57 @@ def validate_add_securities():
     except Exception as e:
         Log.func_error(e=e)
         return HTTPResponse.error(message="Internal Server Error", status_code=500)
+    
+def validate_add_securities():
+    try:
+        req_body = flask.request.get_json()
+        fund_type = req_body.get('fund_type')
+        records = req_body.get("records")
+
+        validation_response = diService.validate_add_securities(records, fund_type)
+        mismatched_data = validation_response.get("data")
+        if validation_response.get("success") and len(mismatched_data) > 0:
+            return HTTPResponse.error(message="File validation failed.", result=mismatched_data, status_code=200, error='VALIDATION_FAILED')
+        
+        return HTTPResponse.success(message="Validation successful.")
+
+    except Exception as e:
+        Log.func_error(e=e)
+        return HTTPResponse.error(message="Internal Server Error", status_code=500)
+    
+def compare_file_columns():
+    try:
+        file = flask.request.files.get('file')
+        fund_type = flask.request.form.get('fund_type')
+
+        company_id = 1
+        file_type = "addsecurities"
+
+        compare_response = diService.compare_columns(file, fund_type, company_id, file_type)
+        if not compare_response.get("success"):
+            return HTTPResponse.error(message="Comparison failed.")
+
+        return HTTPResponse.success(message="Compared successfully.", result=compare_response.get('data'))
+
+    except Exception as e:
+        Log.func_error(e=e)
+        return HTTPResponse.error(message="Internal Server Error", status_code=500)
+    
+def save_mapped_columns():
+    try:
+        req_body = flask.request.get_json()
+        mapped_columns = req_body.get('mapped_columns')
+
+        ids_list =[]
+        for col_info in mapped_columns:
+            ids_list.append(col_info.get("id"))
+        
+        response = diService.save_columns(ids_list, mapped_columns)
+        if not response.get('success'):
+            return HTTPResponse.error(message="Something went wrong.")
+        
+        return HTTPResponse.success(message="Saved successfully.")
+
+    except Exception as e:
+        Log.func_error(e=e)
+        return HTTPResponse.error(message="Internal Server Error", status_code=500)
