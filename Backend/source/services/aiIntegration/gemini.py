12488902de584ba60f21a6a5818b73dae3ba72c1
punit-fm <punit.suman@flairminds.com>
2025-06-04 15:58:08 +0530

    serpapi added

--- Backend/source/services/aiIntegration/gemini.py
+++ Backend/source/services/aiIntegration/gemini.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
 import os
+from serpapi import SerpApiClient
 
 class GeminiClient:
     def __init__(self, api_key=None):
@@ -9,6 +10,25 @@ class GeminiClient:
             raise ValueError("Gemini API key must be provided via argument or GEMINI_API_KEY env variable.")
         genai.configure(api_key=api_key)
         self.model = genai.GenerativeModel('gemini-2.0-flash')
+    
+    def search(self):
+        try:
+            # Define parameters for the SerpApi search
+            params = {
+                "engine": "google",
+                "q": 'Pegasus Steel company details',
+                "api_key": "10ef535b00fe562340523ffa69d27b417a77f1680f14b3c3044dfa448c6739b4",
+                "location": "New York, United States" # Current location, as per your request
+            }
+
+            # Perform the search using SerpApi
+            search = SerpApiClient(params)
+            results = search.get_json()
+            print(results)
+            return results
+
+        except Exception as e:
+            raise Exception(e)
 
     def generate_content(self, prompt: str) -> str:
         """
